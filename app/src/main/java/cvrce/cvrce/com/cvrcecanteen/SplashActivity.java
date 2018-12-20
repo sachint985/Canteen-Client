2ad74a0b4617dde56b3b8981f4f94ac2fe7ae7d1
sourav <souravkumarb175@gmail.com>
2018-12-21 04:30:56 +0530

    Checks for internet connection on start

--- app/src/main/java/cvrce/cvrce/com/cvrcecanteen/SplashActivity.java
+++ app/src/main/java/cvrce/cvrce/com/cvrcecanteen/SplashActivity.java
@@ -1,17 +1,69 @@
 package cvrce.cvrce.com.cvrcecanteen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class SplashActivity extends AppCompatActivity {
 
+    long splashTime=200, time=0;
+
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash);
+
+        final RelativeLayout rl = findViewById(R.id.splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                try{
+                    while(time<splashTime){
+                        time+=10;
+                        //Log.d("debug tag", "here");
+                    }
+                }
+                catch (Exception e){
+
+                }
+               finally{
+                    if(!isOnline()){
+                        Snackbar snackbar = Snackbar.make(rl, "No Internet Connection",
+                                Snackbar.LENGTH_INDEFINITE).setAction("Retry",
+                                new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        recreate();
+                                    }
+                                });
+
+                        snackbar.show();
+                    }
+                    else{
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        };
+
+        thread.start();
+
+    }
+
+    private boolean isOnline(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
     }
 }
