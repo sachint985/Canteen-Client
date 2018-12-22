2b613f5babf474e60502547603544c663c35cc37
sachint985 <sacht985@gmail.com>
2018-12-22 02:43:13 -0800

    splash activity working.. (only if app is in online mode)

--- app/src/main/java/cvrce/cvrce/com/cvrcecanteen/SplashActivity.java
+++ app/src/main/java/cvrce/cvrce/com/cvrcecanteen/SplashActivity.java
@@ -4,61 +4,65 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 public class SplashActivity extends AppCompatActivity {
 
-    long splashTime=200, time=0;
+    final static int SPLASH_TIME_OUT = 3000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        LayoutInflater layoutInflater;
+        ViewGroup root;
+        final View view = (CoordinatorLayout)findViewById(R.id.splash);
 
-        final RelativeLayout rl = findViewById(R.id.splash);
 
-        Thread thread = new Thread() {
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
             @Override
             public void run() {
-                super.run();
+                if(!isOnline()){
+                    Snackbar snackbar = Snackbar.make(view, "No Internet Connection",
+                            Snackbar.LENGTH_INDEFINITE).setAction("Retry",
+                            new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    recreate();
+                                }
+                            });
 
-                try{
-                    while(time<splashTime){
-                        time+=10;
-                        //Log.d("debug tag", "here");
-                    }
+                    snackbar.show();
                 }
-                catch (Exception e){
+                else{
 
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
-               finally{
-                    if(!isOnline()){
-                        Snackbar snackbar = Snackbar.make(rl, "No Internet Connection",
-                                Snackbar.LENGTH_INDEFINITE).setAction("Retry",
-                                new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        recreate();
-                                    }
-                                });
-
-                        snackbar.show();
-                    }
-                    else{
-                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
+                finish();
             }
-        };
+        }, SPLASH_TIME_OUT);
 
-        thread.start();
+
+
+
+
 
     }
 
