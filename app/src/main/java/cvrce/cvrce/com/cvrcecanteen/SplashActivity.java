0584c226364ef1eff348eb8be680bfeb34312135
sourav <souravkumarb175@gmail.com>
2018-12-22 23:20:30 +0530

    splash screen issue fixed

--- app/src/main/java/cvrce/cvrce/com/cvrcecanteen/SplashActivity.java
+++ app/src/main/java/cvrce/cvrce/com/cvrcecanteen/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -22,43 +23,27 @@ public class SplashActivity extends AppCompatActivity {
 
         final RelativeLayout rl = findViewById(R.id.splash);
 
-        Thread thread = new Thread() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                super.run();
+                if(isOnline()){
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
 
-                try{
-                    while(time<splashTime){
-                        time+=10;
-                        //Log.d("debug tag", "here");
-                    }
+                    finish();
                 }
-                catch (Exception e){
-
-                }
-               finally{
-                    if(!isOnline()){
-                        Snackbar snackbar = Snackbar.make(rl, "No Internet Connection",
-                                Snackbar.LENGTH_INDEFINITE).setAction("Retry",
-                                new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        recreate();
-                                    }
-                                });
-
-                        snackbar.show();
-                    }
-                    else{
-                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
+                else{
+                    Snackbar snackbar = Snackbar.make(rl, "No Internet Connection", Snackbar.LENGTH_INDEFINITE).
+                            setAction("Retry", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    recreate();
+                                }
+                            });
+                    snackbar.show();
                 }
             }
-        };
-
-        thread.start();
+        }, 2000);
 
     }
 
