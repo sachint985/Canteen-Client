76a46cdc17f24c1bbf0a006085f79c8e946d636c
sourav <souravkumarb175@gmail.com>
2018-12-23 23:48:13 +0530

    Added icons in navigation drawer menu

--- app/src/main/java/cvrce/cvrce/com/cvrcecanteen/MainActivity.java
+++ app/src/main/java/cvrce/cvrce/com/cvrcecanteen/MainActivity.java
@@ -83,17 +83,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_orderFood) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_prevOrders) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_menu) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_wallet) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_help) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_feedback) {
 
         }
 
